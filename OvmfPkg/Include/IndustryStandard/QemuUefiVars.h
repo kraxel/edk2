f7df6b467a2effdfc6064895a134d543c2cb2489
Gerd Hoffmann <kraxel@redhat.com>
2025-02-14 12:36:24 +0100

    [pio] update register interface header
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- OvmfPkg/Include/IndustryStandard/QemuUefiVars.h
+++ OvmfPkg/Include/IndustryStandard/QemuUefiVars.h
@@ -12,24 +12,33 @@
 /* isa: io range */
 #define UEFI_VARS_IO_BASE  0x520
 
+/* qom: device names */
+#define TYPE_UEFI_VARS_ISA     "uefi-vars-isa"
+#define TYPE_UEFI_VARS_SYSBUS  "uefi-vars-sysbus"
+
 /* sysbus: fdt node path */
 #define UEFI_VARS_FDT_NODE    "qemu-uefi-vars"
 #define UEFI_VARS_FDT_COMPAT  "qemu,uefi-vars"
 
 /* registers */
-#define UEFI_VARS_REG_MAGIC           0x00         /* 16 bit */
-#define UEFI_VARS_REG_CMD_STS         0x02         /* 16 bit */
-#define UEFI_VARS_REG_BUFFER_SIZE     0x04         /* 32 bit */
-#define UEFI_VARS_REG_BUFFER_ADDR_LO  0x08         /* 32 bit */
-#define UEFI_VARS_REG_BUFFER_ADDR_HI  0x0c         /* 32 bit */
-#define UEFI_VARS_REGS_SIZE           0x10
+#define UEFI_VARS_REG_MAGIC                0x00    /* 16 bit */
+#define UEFI_VARS_REG_CMD_STS              0x02    /* 16 bit */
+#define UEFI_VARS_REG_BUFFER_SIZE          0x04    /* 32 bit */
+#define UEFI_VARS_REG_DMA_BUFFER_ADDR_LO   0x08    /* 32 bit */
+#define UEFI_VARS_REG_DMA_BUFFER_ADDR_HI   0x0c    /* 32 bit */
+#define UEFI_VARS_REG_PIO_BUFFER_TRANSFER  0x10    /* 8-64 bit */
+#define UEFI_VARS_REG_PIO_BUFFER_CRC32C    0x18    /* 32 bit (read-only) */
+#define UEFI_VARS_REG_RESERVED             0x1c    /* 32 bit */
+#define UEFI_VARS_REGS_SIZE                0x20
 
 /* magic value */
 #define UEFI_VARS_MAGIC_VALUE  0xef1
 
 /* command values */
-#define UEFI_VARS_CMD_RESET  0x01
-#define UEFI_VARS_CMD_MM     0x02
+#define UEFI_VARS_CMD_RESET            0x01
+#define UEFI_VARS_CMD_DMA_MM           0x02
+#define UEFI_VARS_CMD_PIO_MM           0x03
+#define UEFI_VARS_CMD_PIO_ZERO_OFFSET  0x04
 
 /* status values */
 #define UEFI_VARS_STS_SUCCESS              0x00
